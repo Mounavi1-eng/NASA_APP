187c7fcf003baaff20396969301fe81349ecfc39
Mounavi <90318835+Mounavi1-eng@users.noreply.github.com>
2025-07-02 14:52:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 5000;
 const NASA_API_KEY = process.env.NASA_API_KEY;
 
 // Middleware
-app.use(cors());
+app.use(cors(cors({
+    origin: "https://nasa-frontend-era4.onrender.com", // frontend URL on Render
+  })));
 app.use(express.json());
 
 // APOD route: returns today's Astronomy Picture of the Day
